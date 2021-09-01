c192089a6af3a5d0d1b2fdd6fc4139e336411e68
mul91n <jacob@mulquin.com>
2021-09-01 14:17:42 +1000

    Add proper .gitignore

--- companion_card_directory.py
+++
@@ -1,8 +0,0 @@
-import companion_card_directory as ccd
-
-states = ['nt', 'act']
-
-for state in states:
-    ccd.helpers.make_scrape_dir(state)
-    scrape = getattr(ccd.scrape, state)
-    scrape()
