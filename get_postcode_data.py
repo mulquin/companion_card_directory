cc3a233f009e1331e4173a2c0e59994e7a0a00f3
Jacob Mulquin <jacob@mulquin.com>
2022-08-10 18:55:16 +1000

    Get postcodes data, extract by sa3 region and put into seperate file

--- get_postcode_data.py
+++ get_postcode_data.py
@@ -1,10 +1,32 @@
-import requests
+import companion_card_directory as ccd
+import json
+import os
 
 remote_url = 'https://raw.githubusercontent.com/matthewproctor/australianpostcodes/master/australian_postcodes.json'
-local_file = 'data/postcodes.json'
+postcodes_dir = 'postcodes' + os.path.sep
 
-data = requests.get(remote_url)
+if not os.path.exists(postcodes_dir):
+    os.makedirs(postcodes_dir, exist_ok=True)
+    
+file = ccd.helpers.get_content_from_cache_or_remote(remote_url, ccd.helpers.get_data_dir() + postcodes_dir)
+data = json.loads(file)
 
-with open(local_file, 'wb') as file:
-    file.write(data.content)
+entries = {}
+for record in data:
+    state = record['state'].lower()
 
+    entry = {
+        'state': state,
+        'postcode': record['postcode'],
+        'region': record['SA3_NAME_2016']
+    }
+
+    if state not in entries.keys():
+        entries[state] = {}
+
+    entries[state][entry['postcode']] = entry
+
+for state,entry in entries.items():
+    print(state)
+    print(postcodes_dir + state + '.json')
+    ccd.helpers.write_json_file(postcodes_dir + state + '.json', entry)
